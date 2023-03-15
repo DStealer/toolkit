11a38d55c86d0a0fc974e1c9503a2acf92610e15
LiShiwu <lswu2011@foxmail.com>
2023-03-15 22:48:56 +0800

    bugfix: 遗漏redis命令

---
+++ scripts/springjar.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+java -Djarmode=layertools -jar $(find *-SNAPSHOT.jar) extract
+
+find dependencies/ -exec touch -t 197001010000 {} \;
+find spring-boot-loader/ -exec touch -t 197001010000 {} \;
+find snapshot-dependencies/ -exec touch -t 197001010000 {} \;
+find application/ -exec touch -t 197001010000 {} \;
+
+cat <<EOF >Dockerfile
+FROM repo.gwsl.com:18080/library/jre-8u202-alpine:20221017
+COPY dependencies/ /app
+COPY spring-boot-loader/ /app
+COPY snapshot-dependencies/ /app
+COPY application/ /app
+WORKDIR /app
+CMD ["tini", "--", "java", "org.springframework.boot.loader.JarLauncher"]
+EOF
