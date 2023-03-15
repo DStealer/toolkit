beb0074309c8867b37fb29f7145adedcc4578ccc
LiShiwu <lswu2011@foxmail.com>
2023-03-16 01:47:25 +0800

    feature: 构建命令

--- scripts/springjar.sh
+++ scripts/springjar.sh
@@ -2,7 +2,7 @@
 set -ex
 mvn -e -B -U -pl=./ clean dependency:tree deploy
 
-rm -rf target/dependencies/ target/spring-boot-loader/ target/snapshot-dependencies/ target/application/ target/Dockerfile
+rm -rf target/dependencies/ target/spring-boot-loader/ target/snapshot-dependencies/ target/application/ target/Dockerfile target/idfile.txt target/idfile.txt
 
 java -Duser.dir=$(realpath target) -Djarmode=layertools -jar $(realpath $(find target/*-SNAPSHOT.jar)) extract
 
@@ -23,7 +23,7 @@ EOF
 
 IMAGE_TAG=repo.dstealer.com:18080/library/$(mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout):$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)-$(TZ='Asia/Shanghai' date +'%y%m%d%H%M%S')
 
-buildah build --timestamp=0 --iidfile=target/idfile.txt target
+buildah build --iidfile=target/idfile.txt target
 
 buildah push --rm --digestfile=target/digestfile $(cat target/idfile.txt) docker-daemon:$IMAGE_TAG
 
