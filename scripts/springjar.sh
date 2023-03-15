5541222afd993feb3b9cbf17de33516a412a980d
LiShiwu <lswu2011@foxmail.com>
2023-03-16 01:44:47 +0800

    feature: 构建命令

--- scripts/springjar.sh
+++ scripts/springjar.sh
@@ -4,16 +4,14 @@ mvn -e -B -U -pl=./ clean dependency:tree deploy
 
 rm -rf target/dependencies/ target/spring-boot-loader/ target/snapshot-dependencies/ target/application/ target/Dockerfile
 
-java -Djarmode=layertools -jar $(find target/*-SNAPSHOT.jar) extract
+java -Duser.dir=$(realpath target) -Djarmode=layertools -jar $(realpath $(find target/*-SNAPSHOT.jar)) extract
 
 find target/dependencies/ -exec touch -t 197001010000.00 {} \;
 find target/spring-boot-loader/ -exec touch -t 197001010000.00 {} \;
 find target/snapshot-dependencies/ -exec touch -t 197001010000.00 {} \;
 find target/application/ -exec touch -t 197001010000.00 {} \;
 
-TAG=repo.dstealer.com:18080/library/$(mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout):$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)-$(TZ='Asia/Shanghai' date +'%y%m%d%H%M%S')
-
-cat <<EOF > target/Dockerfile
+cat <<EOF >target/Dockerfile
 FROM repo.dstealer.com:18080/library/jre-8u202-alpine:20221017
 COPY dependencies/ /app
 COPY spring-boot-loader/ /app
@@ -22,3 +20,11 @@ COPY application/ /app
 WORKDIR /app
 CMD ["tini", "--", "java", "org.springframework.boot.loader.JarLauncher"]
 EOF
+
+IMAGE_TAG=repo.dstealer.com:18080/library/$(mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout):$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)-$(TZ='Asia/Shanghai' date +'%y%m%d%H%M%S')
+
+buildah build --timestamp=0 --iidfile=target/idfile.txt target
+
+buildah push --rm --digestfile=target/digestfile $(cat target/idfile.txt) docker-daemon:$IMAGE_TAG
+
+buildah rmi $(cat target/idfile.txt)
