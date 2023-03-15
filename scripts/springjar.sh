41863e0f8154fec7e74502bacadda51c41aa8956
LiShiwu <lswu2011@foxmail.com>
2023-03-16 00:11:24 +0800

    feature: 构建命令

--- scripts/springjar.sh
+++ scripts/springjar.sh
@@ -2,18 +2,18 @@
 set -ex
 mvn -e -B -U -pl=./ clean dependency:tree deploy
 
-rm -rf dependencies/ spring-boot-loader/ snapshot-dependencies/ application/ Dockerfile
+rm -rf target/dependencies/ target/spring-boot-loader/ target/snapshot-dependencies/ target/application/ target/Dockerfile
 
-java -Djarmode=layertools -jar $(find *-SNAPSHOT.jar) extract
+java -Djarmode=layertools -jar $(find target/*-SNAPSHOT.jar) extract
 
-find dependencies/ -exec touch -t 197001010000.00 {} \;
-find spring-boot-loader/ -exec touch -t 197001010000.00 {} \;
-find snapshot-dependencies/ -exec touch -t 197001010000.00 {} \;
-find application/ -exec touch -t 197001010000.00 {} \;
+find target/dependencies/ -exec touch -t 197001010000.00 {} \;
+find target/spring-boot-loader/ -exec touch -t 197001010000.00 {} \;
+find target/snapshot-dependencies/ -exec touch -t 197001010000.00 {} \;
+find target/application/ -exec touch -t 197001010000.00 {} \;
 
 TAG=repo.dstealer.com:18080/library/$(mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout):$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)-$(TZ='Asia/Shanghai' date +'%y%m%d%H%M%S')
 
-cat <<EOF >Dockerfile
+cat <<EOF >target/Dockerfile
 FROM repo.dstealer.com:18080/library/jre-8u202-alpine:20221017
 COPY dependencies/ /app
 COPY spring-boot-loader/ /app
