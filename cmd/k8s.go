6df0cf859548b7a1c2d7f7191be679ba51803b47
LiShiwu <lswu2011@foxmail.com>
2023-05-19 09:49:38 +0800

    feature: 合并功能

--- cmd/k8s.go
+++ cmd/k8s.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"github.com/prometheus/common/log"
 	"github.com/spf13/cobra"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -13,11 +12,15 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 	"k8s.io/client-go/tools/portforward"
 	"k8s.io/client-go/transport/spdy"
+	"k8s.io/client-go/util/homedir"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"time"
 )
 
@@ -30,11 +33,12 @@ var (
 
 func init() {
 
-	k8sCmd.PersistentFlags().String("kubeconfig", "/home/dstealer/.kube/config", "Path to the kubeconfig file to use for CLI requests.")
+	k8sCmd.PersistentFlags().String("kubeconfig", filepath.Join(homedir.HomeDir(), ".kube", "config"), "Path to the kubeconfig file to use for CLI requests.")
 
-	sshdCmd := &cobra.Command{
+	sshCmd := &cobra.Command{
 		Use:   "ssh [args]",
 		Short: "ssh网络分析工具",
+		Args:  cobra.NoArgs,
 		Run: func(cmd *cobra.Command, args []string) {
 			stopChannel := make(chan struct{}, 1)
 			signals := make(chan os.Signal, 1)
@@ -46,19 +50,28 @@ func init() {
 					close(stopChannel)
 				}
 			}()
-			kubeconfig := cmd.Flag("kubeconfig").Value.String()
-			config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+			kubeconfig, err := cmd.Flags().GetString("kubeconfig")
+			cobra.CheckErr(err)
+
+			currentContext, err := cmd.Flags().GetString("context")
+			cobra.CheckErr(err)
+
+			clientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfig}, &clientcmd.ConfigOverrides{CurrentContext: currentContext, ClusterInfo: clientcmdapi.Cluster{InsecureSkipTLSVerify: true}})
+
+			config, err := clientConfig.ClientConfig()
+
 			cobra.CheckErr(err)
 			clientSet := kubernetes.NewForConfigOrDie(config)
 			pod, err := clientSet.CoreV1().Pods("default").Get(context.TODO(), "netshoot", metav1.GetOptions{})
-			image := cmd.Flag("image").Value.String()
+
 			if errors.IsNotFound(err) {
+				image, err := cmd.Flags().GetString("image")
+				cobra.CheckErr(err)
 				pod = &v1.Pod{
 					TypeMeta: metav1.TypeMeta{APIVersion: "v1", Kind: "Pod"},
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      "netshoot",
 						Namespace: "default",
-						Labels:    map[string]string{"app": "netshoot", "tier": "devops"},
 					},
 					Spec: v1.PodSpec{
 						Containers: []v1.Container{{
@@ -66,26 +79,26 @@ func init() {
 							Image:           image,
 							ImagePullPolicy: v1.PullIfNotPresent,
 							Ports: []v1.ContainerPort{
-								{Name: "sshd", ContainerPort: 22, Protocol: v1.ProtocolTCP},
+								{Name: "ssh", ContainerPort: 22, Protocol: v1.ProtocolTCP},
 							},
 							ReadinessProbe: &v1.Probe{
 								Handler: v1.Handler{
 									TCPSocket: &v1.TCPSocketAction{
-										Port: intstr.FromString("sshd"),
+										Port: intstr.FromString("ssh"),
 									},
 								},
 							},
 							LivenessProbe: &v1.Probe{
 								Handler: v1.Handler{
 									TCPSocket: &v1.TCPSocketAction{
-										Port: intstr.FromString("sshd"),
+										Port: intstr.FromString("ssh"),
 									},
 								},
 							},
 							StartupProbe: &v1.Probe{
 								Handler: v1.Handler{
 									TCPSocket: &v1.TCPSocketAction{
-										Port: intstr.FromString("sshd"),
+										Port: intstr.FromString("ssh"),
 									},
 								},
 							},
@@ -96,17 +109,17 @@ func init() {
 						}},
 					},
 				}
-				_, err := clientSet.CoreV1().Pods(pod.Namespace).Create(context.Background(), pod, metav1.CreateOptions{})
+				_, err = clientSet.CoreV1().Pods(pod.Namespace).Create(context.Background(), pod, metav1.CreateOptions{})
 				cobra.CheckErr(err)
 
 				err = wait.PollImmediateUntil(1*time.Second, func() (bool, error) {
 					newPod, err := clientSet.CoreV1().Pods(pod.Namespace).Get(context.TODO(), pod.Name, metav1.GetOptions{})
 					if err != nil {
-						log.Errorf("Error getting Pod :%q [%v]\n", newPod.Name, err)
+						fmt.Fprintf(os.Stderr, "Error getting Pod :%q [%v]\n", newPod.Name, err)
 						return false, nil
 					}
 					if newPod == nil {
-						log.Errorf("Pod :%q not found\n", newPod.Name)
+						fmt.Fprintf(os.Stderr, "Pod :%q not found\n", newPod.Name)
 						return false, nil
 					}
 					if newPod.Status.Phase != v1.PodRunning {
@@ -116,10 +129,7 @@ func init() {
 				}, stopChannel)
 
 				cobra.CheckErr(err)
-
 				pod, err = clientSet.CoreV1().Pods(pod.Namespace).Get(context.TODO(), pod.Name, metav1.GetOptions{})
-				cobra.CheckErr(err)
-			} else {
 				cobra.CheckErr(err)
 			}
 
@@ -138,14 +148,17 @@ func init() {
 			dialer := spdy.NewDialer(upgrader, &http.Client{Transport: transport}, "POST", req.URL())
 
 			readyChannel := make(chan struct{})
-			fw, err := portforward.NewOnAddresses(dialer, []string{"127.0.0.1"}, []string{"22622:22"}, stopChannel, readyChannel, os.Stdout, os.Stderr)
+			localPort := cmd.Flag("local-port").Value.String()
+			fw, err := portforward.NewOnAddresses(dialer, []string{"127.0.0.1"}, []string{fmt.Sprintf("%v:22", localPort)}, stopChannel, readyChannel, os.Stdout, os.Stderr)
 			cobra.CheckErr(err)
-			log.Infof("ssh port listen on 127.0.0.1:%v \n", 22622)
+			log.Printf("ssh port listen on 127.0.0.1:%v \n", localPort)
 			err = fw.ForwardPorts()
 			cobra.CheckErr(err)
 		},
 	}
-	sshdCmd.Flags().String("image", "registry.develop.com:5000/dstealer/netshoot-sshd:latest", "使用的镜像")
-	k8sCmd.AddCommand(sshdCmd)
+	sshCmd.Flags().String("image", "registry.develop.com:5000/library/netshoot-sshd:latest", "使用的镜像")
+	sshCmd.Flags().String("context", "", "当前使用的上下文环境")
+	sshCmd.Flags().Int("local-port", 22622, "使用的本地端口")
+	k8sCmd.AddCommand(sshCmd)
 
 }
