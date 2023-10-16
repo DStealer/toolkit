c1de200275ea54638afa415e3a386fe9e5456b9d
李世伍 <lswu2011@foxmail.com>
2023-10-16 17:21:13 +0800

    调整

--- cmd/go_test.go
+++ cmd/go_test.go
@@ -766,12 +766,17 @@ func TestFixUpPackageLock(t *testing.T) {
 }
 
 func TestBatchUpdate(t *testing.T) {
+	table := "`gwtrip-advertisement`.user_tp_content"
+	update := "update `gwtrip-advertisement`.b2c_faq_userrecord set loginTime=loginTime, enc_mobile=TO_BASE64(AES_ENCRYPT(mobile,'key')) ,gmt_modified=NOW() where id between ? and ?"
+	var sid int64 = 0
+	var eid int64 = 0
+	if eid <= sid {
+		cobra.CheckErr("数据边界错误")
+	}
+
 	conn, err := mysqlclient.Connect("192.168.122.1:3306", "root", "root@123", "information_schema")
 	cobra.CheckErr(err)
 	defer conn.Close()
-
-	table := "`gwtrip-advertisement`.user_tp_content"
-	update := "update `gwtrip-advertisement`.b2c_faq_userrecord set loginTime=loginTime, enc_mobile=TO_BASE64(AES_ENCRYPT(mobile,'key')) ,gmt_modified=NOW() where id between ? and ?"
 
 	result, err := conn.Execute(fmt.Sprintf("SHOW KEYS FROM %s WHERE Key_name = 'PRIMARY' ", table))
 	cobra.CheckErr(err)
@@ -792,8 +797,15 @@ func TestBatchUpdate(t *testing.T) {
 	hid, err := result.GetIntByName(0, "Hid")
 	cobra.CheckErr(err)
 	result.Close()
+	log.Infof("数据上下边界:%v-%v", lid, hid)
+	if sid != 0 && sid > lid {
+		lid = sid
+	}
+	if eid != 0 && eid < hid {
+		hid = eid
+	}
+	log.Infof("调整数据边界:%v-%v", lid, hid)
 
-	fmt.Println(lid, hid)
 	for _, sr := range StepRange(1, 10000001, 250) {
 		result, err = conn.Execute(update, sr.l, sr.r)
 		cobra.CheckErr(err)
