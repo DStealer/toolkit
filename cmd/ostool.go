8b50862bf0b05b3219d49f4951c26a5220ed64e7
李世伍 <lswu2011@foxmail.com>
2024-01-07 18:34:20 +0800

    增加批量执行命令功能

--- cmd/ostool.go
+++ cmd/ostool.go
@@ -97,7 +97,7 @@ func init() {
 			cobra.CheckErr(err)
 			stopIfFailed, err := cmd.Flags().GetBool("stop-if-failed")
 			cobra.CheckErr(err)
-			file, err := os.OpenFile(args[1], os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0666)
+			file, err := os.OpenFile(args[0], os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0666)
 			cobra.CheckErr(err)
 			defer file.Close()
 			br := bufio.NewReader(file)
